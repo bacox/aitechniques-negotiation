ce38dedc9e6b1b61388fdbc733782423bad876f7
niekvanderlaan <niekvanderlaan@outlook.com>
2019-10-10 13:38:34 +0200

    added initial acceptance strategy, needs bidding strategy

--- src/NegotiationAgent.java
+++ src/NegotiationAgent.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.ArrayList;
 
 import genius.core.AgentID;
 import genius.core.Bid;
@@ -11,6 +12,11 @@ import genius.core.parties.NegotiationInfo;
 public class NegotiationAgent extends AbstractNegotiationParty{
 
     private Bid lastReceivedBid = null;
+    private List<Bid> listOfBids = new ArrayList<Bid>();
+    private double totalTimeGiven = 0;
+    private double last15RoundsAvgTime = 0;
+    private double lastBidTimestamp = 0;
+    private int currentPhase = 0;
 
     @Override
     public void init(NegotiationInfo info) {
@@ -22,19 +28,41 @@ public class NegotiationAgent extends AbstractNegotiationParty{
 
         // if you need to initialize some variables, please initialize them
         // below
-
+        this.totalTimeGiven = info.getTimeline().getTotalTime();
+       
     }
 
     @Override
     public Action chooseAction(List<Class<? extends Action>> validActions) {
 
+    	Bid bid = generateRandomBid();
+    	double bidUtil = this.getUtility(bid);
+    	
+    	//If the utility of the bid we were going to do is equal to or lower than
+    	// the utility of the bid just done by the other party, we accept it.
+    	if (lastReceivedBid != null && bidUtil <= this.getUtility(lastReceivedBid)) {
+    		return new Accept(getPartyId(), lastReceivedBid); 
+    	}
+    	
+    	if (currentPhase == 3 && this.numRoundsLeft() <= 15) {
+    		return new Accept(getPartyId(), lastReceivedBid); 
+    	}
+    	
+    	
         // with 50% chance, counter offer
         // if we are the first party, also offer.
         if (lastReceivedBid == null || !validActions.contains(Accept.class) || Math.random() > 0.5) {
-            return new Offer(getPartyId(), generateRandomBid());
+            return this.actionSetTimestamp(new Offer(getPartyId(), bid)); 
         } else {
-            return new Accept(getPartyId(), lastReceivedBid);
+            return this.actionSetTimestamp(new Accept(getPartyId(), lastReceivedBid)); 
         }
+    }
+    
+    //Method to set the timestamp just before choosing an action, allowing us to monitor the time between
+    // bids accurately
+    public Action actionSetTimestamp(Action action) {
+    	this.updateLastBidTimestamp();
+    	return action;
     }
 
     @Override
@@ -42,11 +70,29 @@ public class NegotiationAgent extends AbstractNegotiationParty{
         super.receiveMessage(sender, action);
         if (action instanceof Offer) {
             lastReceivedBid = ((Offer) action).getBid();
+            this.updateLastBidTimestamp();
         }
     }
 
     @Override
     public String getDescription() {
         return "example party group N";
+    }
+    
+    public void updateLastBidTimestamp() {
+    	double currentTime = this.getTimeLine().getCurrentTime();
+    	this.updateLast15RoundsAvgTime(currentTime);
+    	this.lastBidTimestamp = currentTime;
+    }
+    
+    public void updateLast15RoundsAvgTime(double timestamp) {
+    	double timeSpentLastBid = timestamp - this.lastBidTimestamp;
+    	int n = Math.min(this.listOfBids.size(), 15);
+    	this.last15RoundsAvgTime = 
+    			this.last15RoundsAvgTime + (timeSpentLastBid - this.last15RoundsAvgTime) / n;
+    }
+    
+    public double numRoundsLeft() {
+    	return (this.totalTimeGiven - this.getTimeLine().getCurrentTime()) / this.last15RoundsAvgTime;
     }
 }
