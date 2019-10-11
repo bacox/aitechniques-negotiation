044d87e8b379b8347cb5692e1530a44c8446d299
bacox <b.a.cox@student.tudelft.nl>
2019-10-11 19:58:20 +0200

    Clean up code and fix some small errors

--- src/NegotiationAgent.java
+++ src/NegotiationAgent.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.ArrayList;
 
+import agents.anac.y2011.ValueModelAgent.BidList;
 import genius.core.AgentID;
 import genius.core.Bid;
 import genius.core.actions.Accept;
@@ -18,6 +18,8 @@ public class NegotiationAgent extends AbstractNegotiationParty{
     private double totalTimeGiven = 0;
     private double last15RoundsAvgTime = 0;
     private double lastBidTimestamp = 0;
+
+    private boolean firstBidFetch = true;
 //    private int currentPhase = 0;
 
     private enum PHASE {
@@ -28,13 +30,13 @@ public class NegotiationAgent extends AbstractNegotiationParty{
     private double[] phaseFractions = new double[] {28.0/36.0, 7.0/36.0, 1.0/36.0};
     private PHASE currentPhase;
 
-    private ArrayList<Bid> opponentBids = new ArrayList<Bid>();
+    private ArrayList<BidWrapper> opponentBids = new ArrayList<BidWrapper>();
 
     @Override
     public void init(NegotiationInfo info) {
 
         super.init(info);
-        System.out.println("The user model is: " + userModel);
+        System.out.println("The user model is: " + userModel); // This is null
         System.out.println("Discount Factor is " + getUtilitySpace().getDiscountFactor());
         System.out.println("Reservation Value is " + getUtilitySpace().getReservationValueUndiscounted());
         currentPhase = PHASE.ONE;
@@ -52,11 +54,11 @@ public class NegotiationAgent extends AbstractNegotiationParty{
     public double concessionValueByPhase(double time) {
         switch (currentPhase) {
             case ONE:
-                return concessionValue(1.0, (3.0/8.0),timeFractionByPhase(time));
+                return concessionValue(1.0, (1.0/8.0),timeFractionByPhase(time));
             case TWO:
-                return concessionValue((7.0/8.0), (2.0/8.0),timeFractionByPhase(time));
+                return concessionValue((7.0/8.0), (1.0/8.0),timeFractionByPhase(time));
             case THREE:
-                return concessionValue((5.0/8.0), (5.0/8.0),timeFractionByPhase(time));
+                return concessionValue((5.0/8.0), (6.0/8.0),timeFractionByPhase(time));
             default:
                 return 0;
         }
@@ -94,44 +96,61 @@ public class NegotiationAgent extends AbstractNegotiationParty{
         }
     }
 
+
+
+
+    public Bid getBoundedBid(double l) {
+        if(firstBidFetch){
+            Collections.sort(opponentBids);
+            firstBidFetch = false;
+        }
+        for (BidWrapper b : opponentBids) {
+            double utility = b.getUtility();
+            if(utility >= l)
+                return b.getBid();
+        }
+        return null;
+    }
+
     public double upperBound(double l) {
         return l + (0.2 * (1 - l));
     }
 
     public Bid generateBid(double l, double u) {
-//        System.out.println("Start generating bid");
-//        List<Bid> bids = getUserModel().getBidRanking().getBidOrder();
-        boolean bidInRange = false;
         int numberGenerated = 0;
         Bid bid = generateRandomBid();
-        while(!bidInRange) {
+        // Try to generate a good enough bid
+        while(true) {
             double util = this.getUtility(bid);
-//            System.out.println("Util value: " + util);
             if(util >= l && util <= u) {
-                bidInRange = true;
-                System.out.println("Bid in range!");
-
                 break;
             }
+            // In the worst case fall back on a random bid.
             bid = generateRandomBid();
-            if(numberGenerated++ > 100){
+//            If it takes to long stop bid generation
+            if(numberGenerated++ > 1000){
                 try {
+                    // Try to do an impossible bid to prevent premature acceptance due to RNG.
                     bid = getUtilitySpace().getMaxUtilityBid();
-//                    System.out.println("Max bid!");
-
                 } catch(Exception e) {
-                    System.out.println("Random bid!");
+//                    System.out.println("Random bid!");
                 }
-                bidInRange = true;
                 break;
             }
         }
-//        System.out.println("End generating bid");
         return bid;
     }
 
     public Bid generateBid(double l) {
         return generateBid(l, Integer.MAX_VALUE);
+    }
+
+    public Bid generatePhaseOneBid(double l, double u) {
+        if( Math.random() <= 0.7) {
+            return generateBid(l, u);
+        } else {
+            return generateBid(l);
+        }
     }
 
 
@@ -142,43 +161,38 @@ public class NegotiationAgent extends AbstractNegotiationParty{
         double time = getTimeLine().getTime();
         double l = concessionValueByPhase(time);
         double u = upperBound(l);
-//        System.out.println("This is phase " + currentPhase + " " + concessionValueByPhase(time) + " \t " + timeFractionByPhase(time) + "\t [" + l + ", " + u + "]");
         Bid bid = generateRandomBid();
         if (currentPhase == PHASE.ONE) {
-            if( Math.random() <= 0.7) {
-//                System.out.println("There is upper limit");
-                bid = generateBid(l, u);
-            } else {
-//                System.out.println("No upper limit");
-                bid = generateBid(l);
-            }
+            bid = generatePhaseOneBid(l,u);
         } else {
 //            Phase two and three
-            bid = opponentBids.get(1);
+            bid = getBoundedBid(l);
+            if(bid == null) {
+                bid = generatePhaseOneBid(l,u);
+            }
+            System.out.println("In phase " + currentPhase + " sending offer " + getUtility(bid) + "with lower bound " + l);
         }
-
-        double bidUtil = this.getUtility(bid);
-        System.out.println("Utility of our bid: " + bidUtil + "\t\t["+l+",\t"+u+"]");
-//        System.out.println("Utility of last bid received: " + this.getUtility(lastReceivedBid));
-
         try {
-            Thread.sleep(100);
+            Thread.sleep(10);
         } catch (InterruptedException e) {
             System.out.println("Not allowed to sleep");
         }
     	
     	//If the utility of the bid we were going to do is equal to or lower than
     	// the utility of the bid just done by the other party, we accept it.
-    	if (lastReceivedBid != null && bidUtil <= this.getUtility(lastReceivedBid)) {
+    	if (lastReceivedBid != null && l <= this.getUtility(lastReceivedBid)) {
+    	    System.out.println("Accepting bid #1 with phase " + currentPhase + " concession value: " + l + " and bid utility: " + this.getUtility(lastReceivedBid));
     		return new Accept(getPartyId(), lastReceivedBid);
     	}
     	
     	// In phase 3, we accept any bid if we estimate the number of rounds left to be less than 15
     	if (currentPhase == PHASE.THREE && this.numRoundsLeft() <= 15) {
+            System.out.println("Accepting bid #2 with phase " + currentPhase + " concession value: " + l + " and bid utility: " + this.getUtility(lastReceivedBid));
     		return new Accept(getPartyId(), lastReceivedBid);
     	}
     	
     	// Return a new offer
+        System.out.println("New offer: " + getUtility(bid));
         return this.actionSetTimestamp(new Offer(getPartyId(), bid));
     }
     
@@ -198,7 +212,9 @@ public class NegotiationAgent extends AbstractNegotiationParty{
             this.updateLastBidTimestamp();
             if(currentPhase == PHASE.ONE) {
 //                Save opponents bids for phase two and three
-                opponentBids.add(((Offer) action).getBid());
+                Bid b = ((Offer) action).getBid();
+                opponentBids.add(new BidWrapper(b, getUtility(b)));
+//                opponentBids.add(((Offer) action).getBid());
             }
         }
     }
