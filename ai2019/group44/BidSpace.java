d122bc2053a47fd6e03c93c613377c418cd1b69f
Renzo Scholman <renzoscholman@gmail.com>
2019-10-24 13:11:13 +0200

    Added bidspace generation and bid selection using bidspace

---
+++ ai2019/group44/BidSpace.java
@@ -0,0 +1,113 @@
+package group44;
+
+import genius.core.Bid;
+import genius.core.BidIterator;
+import genius.core.utility.AbstractUtilitySpace;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class BidSpace {
+    // Time limit for enumeration and sorting
+    private double limit;
+
+    // Keep track of being finished with enumeration and sorting
+    private boolean finishedEnumeration = false;
+    private boolean finishedSort = false;
+
+    // Iterator as class variable to continue later in case of timeout
+    protected BidIterator iter = null;
+
+    // Holds all generated bids
+    protected List<BidWrapper> allBids = new ArrayList<BidWrapper>();
+
+    // General utility space information
+    protected AbstractUtilitySpace utilitySpace;
+
+    /**
+     * Sets the utility space and timeout and starts bid generation
+     *
+     * @param UtilitySpace
+     * @param timeLimit
+     */
+    public BidSpace(AbstractUtilitySpace UtilitySpace, double timeLimit) {
+        this.limit = timeLimit * 1000000000; // Convert to nanoseconds
+        this.utilitySpace = UtilitySpace;
+        this.iter = new BidIterator(this.utilitySpace.getDomain());
+
+        generateAllBids();
+    }
+
+    /**
+     * Continues bid generation in case it has not finished yet
+     */
+    public void continueGeneration(){
+        if (!this.finishedEnumeration){
+            generateAllBids();
+        } else if(!this.finishedSort){
+            sort(this.limit);
+        }
+    }
+
+    protected void sort(double timeLeft){
+        double start = System.nanoTime();
+        long size = this.allBids.size();
+        double time = size * Math.log(size) * 100; // Sort has n log n complexity, assume 10 million elems / s sorting
+        if (timeLeft - (System.nanoTime() - start) > time ){
+            Collections.sort(this.allBids, Collections.reverseOrder());
+            this.finishedSort = true;
+        }
+    }
+
+    /**
+     * Generates all the possible bids in the domain
+     */
+    public void generateAllBids() {
+        double start = System.nanoTime();
+        while (this.iter.hasNext() && (System.nanoTime() - start) < this.limit) {
+            Bid bid = this.iter.next();
+            try {
+                BidWrapper bidWrapper = new BidWrapper(bid, this.utilitySpace.getUtility(bid));
+                this.allBids.add(bidWrapper);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if(!this.iter.hasNext()){
+            this.finishedEnumeration = true;
+            sort(this.limit - (System.nanoTime() - start)); // Start sorting with timeleft
+        }
+    }
+
+    /**
+     * Returns all bids with utility higher than lowerbound and lower or equal to upperbound
+     *
+     * @param lowerbound
+     * @param upperbound
+     *
+     * @return List<BidWrapper>
+     */
+    public List<BidWrapper> getBidsInRange(double lowerbound, double upperbound){
+        List<BidWrapper> bidsInRange = new ArrayList<BidWrapper>();
+
+//        System.out.println("Getting bids in utility range : " + lowerbound + " to : " + upperbound);
+
+        for (BidWrapper bid : this.allBids){
+            double util = bid.getUtility();
+            // skip rest whilst above upperbound (allBids list is sorted already)
+            if (util > upperbound) continue;
+            if(util <= upperbound && util > lowerbound){
+                bidsInRange.add(bid);
+            }
+            // Stop looking further, rest will all be below lowerbound
+            if (util < lowerbound) break;
+        }
+
+//        System.out.println("Found number of possible bids: "+bidsInRange.size());
+
+        return bidsInRange;
+    }
+}
--- ai2019/group44/Group44_Party.java
+++ ai2019/group44/Group44_Party.java
@@ -12,15 +12,21 @@ import genius.core.parties.AbstractNegotiationParty;
 import genius.core.parties.NegotiationInfo;
 import genius.core.utility.AdditiveUtilitySpace;
 
-public class Group44_Party extends AbstractNegotiationParty{
+public class Group44_Party extends AbstractNegotiationParty {
 
-	private OpponentModel opponentModel;
+    private OpponentModel opponentModel;
+    private ArrayList<BidWrapper> opponentBids = new ArrayList<BidWrapper>();
+
     private Bid lastReceivedBid = null;
+    private BidSpace bidSpace = null;
 
     private int numberOfBids = 0;
+
     private double totalTimeGiven = 0;
     private double last15RoundsAvgTime = 0;
     private double lastBidTimestamp = 0;
+    private double epsilon = 0.2;
+    private double[] phaseFractions = new double[]{28.0 / 36.0, 7.0 / 36.0, 1.0 / 36.0};
 
     private boolean firstBidFetch = true;
 
@@ -29,13 +35,8 @@ public class Group44_Party extends AbstractNegotiationParty{
         TWO,
         THREE;
     }
-    
-    private double[] phaseFractions = new double[] {28.0/36.0, 7.0/36.0, 1.0/36.0};
-    private PHASE currentPhase;
 
-    private ArrayList<BidWrapper> opponentBids = new ArrayList<BidWrapper>();
-    
-    private double epsilon = 0.2;
+    private PHASE currentPhase;
 
     @Override
     public void init(NegotiationInfo info) {
@@ -46,16 +47,15 @@ public class Group44_Party extends AbstractNegotiationParty{
         System.out.println("Reservation Value is " + getUtilitySpace().getReservationValueUndiscounted());
         currentPhase = PHASE.ONE;
 
-        // if you need to initialize some variables, please initialize them
-        // below
+        // If you need to initialize some variables, please initialize them below
         this.totalTimeGiven = info.getTimeline().getTotalTime();
-        
-      //Initialize opponent model
-        
-      this.opponentModel = new OpponentModel(((AdditiveUtilitySpace) info.getUtilitySpace()), this.epsilon);
+
+        // Initialize opponent model
+        this.opponentModel = new OpponentModel(((AdditiveUtilitySpace) info.getUtilitySpace()), this.epsilon);
+
+        // Generate our bidspace with a time limit of 45 seconds
+        this.bidSpace = new BidSpace(info.getUtilitySpace(), 45);
     }
-
-
 
     public double concessionValue(double start, double range, double fraction) {
         return start - (range * fraction);
@@ -64,11 +64,11 @@ public class Group44_Party extends AbstractNegotiationParty{
     public double concessionValueByPhase(double time) {
         switch (currentPhase) {
             case ONE:
-                return concessionValue(1.0, (1.0/8.0),timeFractionByPhase(time));
+                return concessionValue(1.0, (1.0 / 8.0), timeFractionByPhase(time));
             case TWO:
-                return concessionValue((7.0/8.0), (1.0/8.0),timeFractionByPhase(time));
+                return concessionValue((7.0 / 8.0), (1.0 / 8.0), timeFractionByPhase(time));
             case THREE:
-                return concessionValue((5.0/8.0), (6.0/8.0),timeFractionByPhase(time));
+                return concessionValue((5.0 / 8.0), (6.0 / 8.0), timeFractionByPhase(time));
             default:
                 return 0;
         }
@@ -79,9 +79,9 @@ public class Group44_Party extends AbstractNegotiationParty{
             case ONE:
                 return time / phaseFractions[0];
             case TWO:
-                return (time- phaseFractions[0]) / (phaseFractions[1]);
+                return (time - phaseFractions[0]) / (phaseFractions[1]);
             case THREE:
-                return (time- phaseFractions[0] - phaseFractions[1]) / (phaseFractions[2]);
+                return (time - phaseFractions[0] - phaseFractions[1]) / (phaseFractions[2]);
             default:
                 return 0;
         }
@@ -90,12 +90,12 @@ public class Group44_Party extends AbstractNegotiationParty{
     public void updatePhase(double time) {
         switch (currentPhase) {
             case ONE:
-                if( time > phaseFractions[0]) {
+                if (time > phaseFractions[0]) {
                     currentPhase = PHASE.TWO;
                 }
                 break;
             case TWO:
-                if( time > phaseFractions[0] + phaseFractions[1]) {
+                if (time > phaseFractions[0] + phaseFractions[1]) {
                     currentPhase = PHASE.THREE;
                 }
                 break;
@@ -105,17 +105,15 @@ public class Group44_Party extends AbstractNegotiationParty{
         }
     }
 
-    
-
 
     public Bid getBoundedBid(double l) {
-        if(firstBidFetch){
+        if (firstBidFetch) {
             Collections.sort(opponentBids);
             firstBidFetch = false;
         }
         for (BidWrapper b : opponentBids) {
             double utility = b.getUtility();
-            if(utility >= l)
+            if (utility >= l)
                 return b.getBid();
         }
         return null;
@@ -126,36 +124,28 @@ public class Group44_Party extends AbstractNegotiationParty{
     }
 
     public Bid generateBid(double l, double u) {
-        int numberGenerated = 0;
-        Bid bid = generateRandomBid();
-        // Try to generate a good enough bid
-        while(true) {
-            double util = this.getUtility(bid);
-            if(util >= l && util <= u) {
-                break;
+        Bid bid = null;
+        List<BidWrapper> possibilities = this.bidSpace.getBidsInRange(l, u);
+        if(possibilities.size() == 0){
+            try {
+                // If the range is too small, just bid the max utility
+                bid = getUtilitySpace().getMaxUtilityBid();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            // In the worst case fall back on a random bid.
-            bid = generateRandomBid();
-//            If it takes to long stop bid generation
-            if(numberGenerated++ > 1000){
-                try {
-                    // Try to do an impossible bid to prevent premature acceptance due to RNG.
-                    bid = getUtilitySpace().getMaxUtilityBid();
-                } catch(Exception e) {
-//                    System.out.println("Random bid!");
-                }
-                break;
-            }
+        } else {
+            // Get random bid in our possible utility range
+            bid = possibilities.get(new Random().nextInt(possibilities.size())).getBid();
         }
         return bid;
     }
 
     public Bid generateBid(double l) {
-        return generateBid(l, Integer.MAX_VALUE);
+        return generateBid(l, 1.0);
     }
 
     public Bid generatePhaseOneBid(double l, double u) {
-        if( Math.random() <= 0.7) {
+        if (Math.random() <= 0.7) {
             return generateBid(l, u);
         } else {
             return generateBid(l);
@@ -163,57 +153,58 @@ public class Group44_Party extends AbstractNegotiationParty{
     }
 
 
-
     @Override
     public Action chooseAction(List<Class<? extends Action>> validActions) {
+        // Continue bidspace generation if needed.
+        this.bidSpace.continueGeneration();
 
-        double time = getTimeLine().getTime();
-        double l = concessionValueByPhase(time);
+        // Setup current actions time lower and upperbound
+        double l = concessionValueByPhase(getTimeLine().getTime());
         double u = upperBound(l);
-        Bid bid = generateRandomBid();
+        Bid bid = null;
         if (currentPhase == PHASE.ONE) {
-            bid = generatePhaseOneBid(l,u);
+            bid = generatePhaseOneBid(l, u);
         } else {
 //            Phase two and three
             bid = getBoundedBid(l);
-            if(bid == null) {
-                bid = generatePhaseOneBid(l,u);
+            if (bid == null) {
+                bid = generatePhaseOneBid(l, u);
             }
             System.out.println("In phase " + currentPhase + " sending offer " + getUtility(bid) + "with lower bound " + l);
         }
-        
+
         double estimatedOpponentUtil = this.opponentModel.estimateOpponentUtility(bid);
 //        System.out.println("estimated opponent utility: " + estimatedOpponentUtil);
-        
+
         try {
             Thread.sleep(10);
         } catch (InterruptedException e) {
             System.out.println("Not allowed to sleep");
         }
-    	
-    	//If the utility of the bid we were going to do is equal to or lower than
-    	// the utility of the bid just done by the other party, we accept it.
-    	if (lastReceivedBid != null && l <= this.getUtility(lastReceivedBid)) {
-    	    System.out.println("Accepting bid #1 with phase " + currentPhase + " concession value: " + l + " and bid utility: " + this.getUtility(lastReceivedBid));
-    		return new Accept(getPartyId(), lastReceivedBid);
-    	}
-    	
-    	// In phase 3, we accept any bid if we estimate the number of rounds left to be less than 15
-    	if (currentPhase == PHASE.THREE && this.numRoundsLeft() <= 15) {
+
+        //If the utility of the bid we were going to do is equal to or lower than
+        // the utility of the bid just done by the other party, we accept it.
+        if (lastReceivedBid != null && l <= this.getUtility(lastReceivedBid)) {
+            System.out.println("Accepting bid #1 with phase " + currentPhase + " concession value: " + l + " and bid utility: " + this.getUtility(lastReceivedBid));
+            return new Accept(getPartyId(), lastReceivedBid);
+        }
+
+        // In phase 3, we accept any bid if we estimate the number of rounds left to be less than 15
+        if (currentPhase == PHASE.THREE && this.numRoundsLeft() <= 15) {
             System.out.println("Accepting bid #2 with phase " + currentPhase + " concession value: " + l + " and bid utility: " + this.getUtility(lastReceivedBid));
-    		return new Accept(getPartyId(), lastReceivedBid);
-    	}
-    	
-    	// Return a new offer
+            return new Accept(getPartyId(), lastReceivedBid);
+        }
+
+        // Return a new offer
         System.out.println("New offer: " + getUtility(bid));
         return this.actionSetTimestamp(new Offer(getPartyId(), bid));
     }
-    
+
     //Method to set the timestamp just before choosing an action, allowing us to monitor the time between
     // bids accurately
     public Action actionSetTimestamp(Action action) {
-    	this.updateLastBidTimestamp();
-    	return action;
+        this.updateLastBidTimestamp();
+        return action;
     }
 
     @Override
@@ -222,13 +213,13 @@ public class Group44_Party extends AbstractNegotiationParty{
         double time = getTimeLine().getTime();
         updatePhase(time);
         if (action instanceof Offer) {
-        	Bid newBid = ((Offer) action).getBid();
-        	if (lastReceivedBid != null) {
-        		this.opponentModel.update(lastReceivedBid, newBid, time);
-        	}
+            Bid newBid = ((Offer) action).getBid();
+            if (lastReceivedBid != null) {
+                this.opponentModel.update(lastReceivedBid, newBid, time);
+            }
             lastReceivedBid = newBid;
             this.updateLastBidTimestamp();
-            if(currentPhase == PHASE.ONE) {
+            if (currentPhase == PHASE.ONE) {
 //                Save opponents bids for phase two and three
                 Bid b = ((Offer) action).getBid();
                 opponentBids.add(new BidWrapper(b, getUtility(b)));
@@ -241,29 +232,29 @@ public class Group44_Party extends AbstractNegotiationParty{
     public String getDescription() {
         return "The negotiation agent of group 44";
     }
-    
+
     public void updateLastBidTimestamp() {
-    	numberOfBids++;
-    	
-    	double currentTime = this.getTimeLine().getCurrentTime();
-    	this.updateLast15RoundsAvgTime(currentTime);
-    	
+        numberOfBids++;
+
+        double currentTime = this.getTimeLine().getCurrentTime();
+        this.updateLast15RoundsAvgTime(currentTime);
+
 //    	System.out.println(("Last bid timestamp: " + this.lastBidTimestamp));
-    	this.lastBidTimestamp = currentTime;
+        this.lastBidTimestamp = currentTime;
 //    	System.out.println(("New bid timestamp: " + this.lastBidTimestamp));
 
     }
-    
+
     public void updateLast15RoundsAvgTime(double timestamp) {
-    	double timeSpentLastBid = timestamp - this.lastBidTimestamp;
-    	int n = Math.min(numberOfBids, 15);
-    	this.last15RoundsAvgTime = 
-    			this.last15RoundsAvgTime + (timeSpentLastBid - this.last15RoundsAvgTime) / n;
-    	
+        double timeSpentLastBid = timestamp - this.lastBidTimestamp;
+        int n = Math.min(numberOfBids, 15);
+        this.last15RoundsAvgTime =
+                this.last15RoundsAvgTime + (timeSpentLastBid - this.last15RoundsAvgTime) / n;
+
 //    	System.out.println(("last15RoundsAvgTime: " + this.last15RoundsAvgTime));
     }
-    
+
     public double numRoundsLeft() {
-    	return (this.totalTimeGiven - this.getTimeLine().getCurrentTime()) / this.last15RoundsAvgTime;
+        return (this.totalTimeGiven - this.getTimeLine().getCurrentTime()) / this.last15RoundsAvgTime;
     }
 }
