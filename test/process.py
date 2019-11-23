cd48801298d5d3ec6dfbe5f7115e7e14749aefd8
Renzo Scholman <renzoscholman@gmail.com>
2019-11-23 22:13:29 +0100

    Updated test & process scripts

--- test/process.py
+++ test/process.py
@@ -16,6 +16,15 @@ def count(data, value):
             amount += 1
     return amount
 
+def accumulate_by_agent(data, column, agents):
+    unique_agents = unique(agents)
+    values = np.asarray(data[column])
+    accumulated = [[] for _ in range(len(unique_agents))]
+    for i in range(len(values)):
+        accumulated[unique_agents.index(agents[i].split('@')[0])].append(values[i])
+    return accumulated
+
+
 parser = argparse.ArgumentParser(description='Processes csv files created by genius.')
 parser.add_argument('-f', required=True)
 args = parser.parse_args()
@@ -27,19 +36,19 @@ if not path.exists(args.f):
 data = pd.read_csv(args.f, delimiter=';')
 agent1 = data['Agent 1']
 agent2 = data['Agent 2']
-
-util1 = np.asarray(data['Utility 1'])
-util2 = np.asarray(data['Utility 2'])
-
 agents = unique(agent1)
-utilities = [[] for _ in range(len(agents))]
-for i in range(len(util1)):
-    utilities[agents.index(agent1[i].split('@')[0])].append(util1[i])
-    utilities[agents.index(agent2[i].split('@')[0])].append(util2[i])
 
-mean_util = np.mean(np.asarray(utilities), axis=1)
-std_util = np.std(np.asarray(utilities), axis=1)
+util1 = accumulate_by_agent(data, 'Utility 1', agent1)
+util2 = accumulate_by_agent(data, 'Utility 2', agent2)
+
+# Do this with map, because our agent (Group44_Party) has more entries then the rest, np.mean will give an error
+util1_mean = list(map(lambda x: np.mean(x), util1))
+util2_mean = list(map(lambda x: np.mean(x), util2))
+util1_std = list(map(lambda x: np.std(x), util2))
+util2_std = list(map(lambda x: np.std(x), util2))
 print(agents)
-print(mean_util)
-print(std_util)
+print(util1_mean)
+print(util1_std)
+print(util2_mean)
+print(util2_std)
 print("Negotiations per agent: ", count(agent1, agents[0])+count(agent2, agents[0]))
--- test/tests.sh
+++ test/tests.sh
@@ -8,10 +8,13 @@ agents=(
     'agents.anac.y2011.HardHeaded.KLH'
 )
 
+# Change this to the location of your compiled class files
 our_agent='E:\UNI\Master\AIT\aitechniques-negotiation\out\production\aitechniques-negotiation\group44\Group44_Party.class'
+our_old_agent='E:\UNI\Master\AIT\aitechniques-negotiation\out\production\aitechniques-negotiation\group44\Group44_Party_old.class'
 
 declare -A domains
 declare -A names
+
 # Party domain
 domains[0,0]='etc/templates/partydomain/party1_utility.xml'
 domains[0,1]='etc/templates/partydomain/party2_utility.xml'
@@ -32,7 +35,7 @@ domains[3,0]='etc/templates/Jobs/Jobs_util1.xml'
 domains[3,1]='etc/templates/Jobs/Jobs_util2.xml'
 names[3]='jobs'
 
-
+# Loop over all 4 domains (or change to x domains if more are added/removed above)
 for ((i=0;i<4;i=i+1)); do
     PART=""
     PREF=""
@@ -43,14 +46,14 @@ for ((i=0;i<4;i=i+1)); do
     for ((j=0;j<2;j=j+1)); do
         PREF="${PREF} ${domains[$i,$j]}"
     done
-    python session_xml.py --parties $PART --preferences $PREF -dt ROUND --repeat 4
+    python session_xml.py --agent $our_agent --parties $PART --preferences $PREF --repeat 5
     cd ../dependency
     java.exe -cp ../dependency/genius-9.1.11.jar genius.cli.Runner ../test/test_tournament.xml ../test/results
     cd ../test
     cat results.csv | sed '1d' > results_${names[$i]}.csv
     rm results.csv
 
-    python session_xml.py --parties $our_agent $our_agent --preferences $PREF -dt ROUND --repeat 10
+    python session_xml.py --agent $our_agent --parties $our_agent $our_old_agent --preferences $PREF --repeat 10
     cd ../dependency
     java.exe -cp ../dependency/genius-9.1.11.jar genius.cli.Runner ../test/test_tournament.xml ../test/results
     cd ../test
